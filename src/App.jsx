1ce184d7be6397912465e2e457b831278a700632
Abbos29 <abbosvaliev2003@gmail.com>
2024-02-01 23:36:02 +0500

    feat: add protfolio half section + add swiper js

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header/Header'
 import './Reset.scss'
 import './App.scss'
 import About from './components/About/About'
+import Portfolio from './components/Portfolio/Portfolio'
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
     <>
       <Header />
       <About />
+      <Portfolio />
 
     </>
   )
